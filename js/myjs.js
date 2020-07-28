28cadeccee9b656767d580437e67d20824399c8e
Martin Goiriz <martingoiriz@gmail.com>
2020-07-27 21:32:15 -0300

    first commit

---
+++ js/myjs.js
@@ -0,0 +1,19 @@
+// document.getElementById("ahorros").click(function() {
+//   window.open(url);
+// });
+
+$(".mg").on("click", function() {
+  window.location = "https://www.martingoiriz.com.ar/";
+});
+
+$(".ahorros").on("click", function() {
+  window.location = "https://www.martingoiriz.com.ar/ahorros-app";
+});
+
+$(".lin").on("click", function() {
+  window.location = "https://www.martingoiriz.com.ar/lin";
+});
+
+$(".empanapp").on("click", function() {
+  window.location = "https://www.martingoiriz.com.ar/empanapp";
+});
